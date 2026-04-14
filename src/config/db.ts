a38cd73aeef86d9eb4cba06433746960086402ef
Mahmoud Solaiman <mahmoudalshahat420@gmail.com>
2026-04-14 11:27:10 +0200

    Setting the project structure and initializing git

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    console.log('Databased connected', conn.connection.host);
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+    process.exit(1);
+  }
+} 
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import dns from 'node:dns';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+import app from "./app";
+
+dns.setServers(['8.8.8.8', '1.1.1.1']); 
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
