7fa485eccdba3b406109d98d56376880860dfa00
Mahmoud Solaiman <mahmoudalshahat420@gmail.com>
2026-04-17 07:53:19 +0200

    Adding the update and the delete idea functionality and completing the CRUD app

--- src/controllers/idea.controller.ts
+++ src/controllers/idea.controller.ts
@@ -50,3 +50,41 @@ export const getIdeaById = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Server error', error});
   }
 }
+
+
+export const updateIdea = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const idea = await Idea.findById(id);
+
+    if (!idea) {
+      return res.status(404).json({ message: 'Idea not found'});
+    }
+
+    idea.title = title || idea.title;
+    idea.description = description || idea.description;
+
+    const updatedIdea = await idea.save();
+
+    res.status(200).json(updatedIdea);
+    
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error});
+  }
+}
+
+export const deleteIdea = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deletedIdea = await Idea.findByIdAndDelete(id);
+
+    if (!deletedIdea) return res.status(404).json({ message: 'Idea not found'});
+
+    res.status(200).json({ message: 'Idea deleted successfully'});
+
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error});
+  }
+}
--- src/routes/idea.routes.ts
+++ src/routes/idea.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { createIdea, getIdeaById, getIdeas } from '../controllers/idea.controller';
+import { createIdea, deleteIdea, getIdeaById, getIdeas, updateIdea } from '../controllers/idea.controller';
 
 const router = express.Router();
 
 router.post('/', createIdea);
 router.get('/', getIdeas);
 router.get('/:id', getIdeaById);
+router.put('/:id', updateIdea);
+router.delete('/:id', deleteIdea);
 
 export default router;
